7331b21a154b6212a8f347ea3d903b74fb6eb44a
sjrustle <sjasonrussell@gmail.com>
2015-04-06 23:05:45 -0400

    got post to work

--- js/parkItMongo.js
+++ js/parkItMongo.js
@@ -6,9 +6,11 @@ function send(){
 	'userName': $('#exampleInputEmail1').val(),
 	'passWord': $('#exampleInputPassword1').val()
 	}
-	var xhr = new XMLHttpRequest();
-    	xhr.open("POST", "https://api.mongolab.com/api/1/databases/parkit/collection/StudentUsers?apiKey=X_7L-M-OY0e-hjHbhVsIM-JLeorp_-xG", true);
-    	xhr.setRequestHeader("Content-Type", "application/json");
-    	xhr.send(json);
+
+	$.ajax( { url: "https://api.mongolab.com/api/1/databases/parkit/collections/StudentUsers?apiKey=X_7L-M-OY0e-hjHbhVsIM-JLeorp_-xG",
+		  data: JSON.stringify( [{'userName': $('#exampleInputEmail1').val()}, {'passWord': $('#exampleInputPassword1').val()} ]),
+		  type: "POST",
+		  contentType: "application/json" } );
+	
 }
 
