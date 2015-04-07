9ad9e0ac5b6ea94c981eb07e3a434ce4d151346c
sjrustle <sjasonrussell@gmail.com>
2015-04-06 22:45:10 -0400

    JSON object is made

--- js/parkItMongo.js
+++ js/parkItMongo.js
@@ -1,11 +1,14 @@
 
+var json;
 
-
-function send(json){
-	var formData = JSON.parse(JSON.stringify(jQuery(json).serializeArray()))
+function send(){
+	json = {
+	'userName': $('#exampleInputEmail1').val(),
+	'passWord': $('#exampleInputPassword1').val()
+	}
 	var xhr = new XMLHttpRequest();
     	xhr.open("POST", "https://api.mongolab.com/api/1/databases/parkit/collection/StudentUsers?apiKey=X_7L-M-OY0e-hjHbhVsIM-JLeorp_-xG", true);
     	xhr.setRequestHeader("Content-Type", "application/json");
-    	xhr.send(formData);
+    	xhr.send(json);
 }
 
