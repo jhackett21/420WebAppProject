b57ca12a7636fca7f16c9ff7e3e9dad6f7a850e1
sjrustle <sjasonrussell@gmail.com>
2015-04-17 22:47:30 -0400

    Rest and Angular now GET

--- js/parkItMongo.js
+++ js/parkItMongo.js
@@ -1,6 +1,59 @@
 
 var json;
 
+function mongoPOST(){
+
+
+	var validEmail = false;
+	validEmail = validate();
+
+	//var userCar = $('car').append($('#exampleInputEmail1').val());
+
+	//Creates JSON object that will be sent to the MongoDB
+
+
+	//MONGOLAB API
+	//if(validEmail ===  true){
+		$.ajax( { url: "https://api.mongolab.com/api/1/databases/parkit/collections/ParkingLot?apiKey=X_7L-M-OY0e-hjHbhVsIM-JLeorp_-xG",
+			  data: JSON.stringify({
+
+			  	userName:$('#exampleInputEmail1').val(),
+			  	passWord: $('#exampleInputPassword1').val(),
+			  	fullName: $('#fullName').val(),
+			  	//vehicleID: userCar
+
+			  }),
+			  type: "POST",
+			  contentType: "application/json" } );
+	//}
+
+}
+
+function mongoGet(){
+
+
+
+
+
+	//MONGOLAB API
+	//if(validEmail ===  true){  https://api.mongolab.com/api/1/databases/my-db/collections?apiKey=myAPIKey
+		$.ajax( { url: "https://api.mongolab.com/api/1/databases/parkit/collections/ParkingLot?apiKey=X_7L-M-OY0e-hjHbhVsIM-JLeorp_-xG",
+			  data: JSON.stringify({
+
+			  	"lotName": "lotName",
+    				"parkingSpot": "number",
+    				"Filled": "false"
+
+			  	//vehicleID: userCar
+
+			  }),
+			  type: "GET",
+			  contentType: "application/json" } );
+	//}
+
+}
+
+
 function send(){
 
 
--- js/resources.js
+++ js/resources.js
@@ -3,18 +3,9 @@
 
     var app = angular.module('parking',[ ]);
 
-    app.controller('ParkingController', function(){
-        this.spot = parkingSpot;
-
-    });
-
-    var parkingSpot = {
-
-        name: 'WoodLand',
-        parkingAva: false
-
-
-    };
-
-
+    app.controller('ParkingController', function($scope, $http) {
+            $http.get("https://api.mongolab.com/api/1/databases/parkit/collections/ParkingLot?apiKey=X_7L-M-OY0e-hjHbhVsIM-JLeorp_-xG")
+            .success(function(response)
+                {$scope.spot = response;});
+             });
 })();
