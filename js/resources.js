3f2ce6c8bc26641be0fe811fadeec3b663acd034
sjrustle <sjasonrussell@gmail.com>
2015-04-22 19:15:26 -0400

    Updated users page with databinding

--- js/resources.js
+++ js/resources.js
@@ -1,11 +1,27 @@
 (function(){
 
 
-    var app = angular.module('parking',[ ]);
+    var app = angular.module('parking',[]);
 
     app.controller('ParkingController', function($scope, $http) {
+            $http.get("https://api.mongolab.com/api/1/databases/parkit/collections/StudentUsers?apiKey=X_7L-M-OY0e-hjHbhVsIM-JLeorp_-xG")
+            .success(function(response)
+                {$scope.user = response;});
+             });
+
+    app.controller('getCars', function($scope, $http) {
+            $http.get("https://api.mongolab.com/api/1/databases/parkit/collections/Car?apiKey=X_7L-M-OY0e-hjHbhVsIM-JLeorp_-xG")
+            .success(function(response)
+                {$scope.cars = response;});
+             });
+
+    app.controller('getSpot', function($scope, $http) {
             $http.get("https://api.mongolab.com/api/1/databases/parkit/collections/ParkingLot?apiKey=X_7L-M-OY0e-hjHbhVsIM-JLeorp_-xG")
             .success(function(response)
                 {$scope.spot = response;});
              });
+
+
+
+
 })();
