6dcb256299895118f51985b61af063adfb19d3a9
sjrustle <sjasonrussell@gmail.com>
2015-04-25 22:41:11 -0400

    Angular and ajax

--- js/resources.js
+++ js/resources.js
@@ -15,11 +15,30 @@
                 {$scope.cars = response;});
              });
 
-    app.controller('getSpot', function($scope, $http) {
+        app.controller('getSpot', function($scope, $http) {
             $http.get("https://api.mongolab.com/api/1/databases/parkit/collections/ParkingLot?apiKey=X_7L-M-OY0e-hjHbhVsIM-JLeorp_-xG")
             .success(function(response)
                 {$scope.spot = response;});
+
+
+            $scope.put = function(response) {
+              $http.put("https://api.mongolab.com/api/1/databases/parkit/collections/ParkingLot/"+response._id.$oid+"?apiKey=X_7L-M-OY0e-hjHbhVsIM-JLeorp_-xG", {"lotName": response.lotName,
+                    "spotNumber": response.spotNumber,
+                "vacant":false,}).success(function(result) {
+                  $scope.currentParking = response;
+                  console.log(response);
+              }).error(function() {
+                  console.log("error");
+              });
+
+            };
+
+
+
              });
+
+
+
 
 
 
